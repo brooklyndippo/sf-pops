552fa4c2497aa058cd449d4cf1ff815639395e3f
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-02-18 22:56:49 -1000

    built header component

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+// src/Title.js
+import React from 'react'
+import './Title.css';
+
+export default function Title() {
+  return (
+  <div className="Title">
+      <h1>SFPOPOS</h1>
+		<div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+  </div>  
+  )
+}
+  
+// src/Title.js

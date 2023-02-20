41c6a527f57c9ebe126a1a57e89be381343cf36c
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-02-19 23:29:01 -1000

    add search capabilities

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,13 +1,23 @@
 import POPOSSpace from "../POPOSSpace/POPOSSpace"
 import './POPOSList.css';
-import data from '../../sfpopos-data.json'
+import data from '../../sfpopos-data.js'
+import { useState } from 'react'
   
 function POPOSList() {
-    const spaces = data.map(( { title, address, images, hours }, i ) => {
-        return (
+  const [ query, setQuery ] = useState('')
+  const spaces = data.filter((obj) => {
+    // true if query is in title
+    const inTitle = obj.title.toLowerCase().includes(query.toLowerCase())
+    // true if query is in address
+    const inAddress = obj.address.toLowerCase().includes(query.toLowerCase())
+    // return true if either is true
+    return inTitle || inAddress
+  })
+  .map(({ title, address, images, hours, id }) => {        
+    return (
           <POPOSSpace
-            id={i}
-            key={title}
+            id={id}
+            key={`${title}-${id}`}
             name={title}
             address={address}
             image={images[0]}
@@ -17,8 +27,18 @@ function POPOSList() {
     });
     
     return (
-    <div className="POPOSList">
-        { spaces }
+    <div>
+      <form>
+        <input
+          value={query}
+          placeholder="search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="POPOSList">
+          { spaces }
+      </div>
     </div>
     )
 };

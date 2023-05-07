58ebf1fb75b6803eabae014e481dcaf6f335727f
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-05-06 21:27:19 -1000

    add images to space page

--- src/components/POPOSDetails/POPOSDetails.jsx
+++ src/components/POPOSDetails/POPOSDetails.jsx
@@ -5,7 +5,7 @@ import data from '../../sfpopos-data';
 // eslint-disable-next-line import/no-cycle
 import { POPOSFeature } from '..';
 
-import './POPOSDetails.css';
+import BackButton from '../BackButton/BackButton';
 
 function POPOSDetails() {
   const params = useParams();
@@ -18,28 +18,34 @@ function POPOSDetails() {
     <div className="POPOSFrame">
       <div className="POPOSDetails">
         <div className="POPOSImage">
-          <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title} />
+          <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt={title} />
         </div>
-
         <div className="POPOSContent">
-          <h1>{ title }</h1>
-          <p>{ desc }</p>
+          <h1 className="bg-white text-4xl text-center my-6 font-black border-[2px] border-black p-4 w-full md:text-left md:p-0 md:pt-20 md:border-l-0 md:border-r-0 md:border-t-0 md:w-fit md:pr-16 md:pb-8 md:border-gray-300 md:border-4">
+            {title}
+          </h1>
+          <p>{desc}</p>
           <p>Features:</p>
           <ul>
-            { features.map((feature) => (<li><POPOSFeature feature={feature} showText /></li>))}
+            {features.map((feature) => (
+              <li>
+                <POPOSFeature feature={feature} showText />
+              </li>
+            ))}
           </ul>
-          <hr />
-          <p>{ hours ? `🕒 ${hours}` : 'Hours not available.' }</p>
-          <p>
-            📍 Latitude
-            { geo.lat }
-            {' '}
-            , Longitude
-            { geo.lon }
-          </p>
+          <div className="border p-10">
+            <p>{hours ? `🕒 ${hours}` : 'Hours not available.'}</p>
+            <p>
+              📍 Latitude
+              {geo.lat}
+              {' '}
+              , Longitude
+              {geo.lon}
+            </p>
+          </div>
         </div>
-
       </div>
+      <BackButton />
     </div>
   );
 }

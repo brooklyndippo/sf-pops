f65a6a55ceb4788796fa3d419c08b71d3c1f08c6
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-05-06 15:18:01 -1000

    hero is responsive and searchbar renders in hero or below depending on screensize

--- src/components/POPOSList/POPOSList.jsx
+++ src/components/POPOSList/POPOSList.jsx
@@ -28,28 +28,32 @@ function POPOSList() {
       />
     ));
 
+  const searchBar = (
+    <form className="flex flex-col my-4 gap-4">
+      <input
+        className="border rounded p-2 w-full"
+        value={query}
+        placeholder="Search for a space..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+
   return (
     <div>
-      <div id="hero" className="relative mb-56">
+      <div id="hero" className="relative mb-52 md:mb-10 md:flex md:flex-row-reverse gap-10">
         <img
           src={`${process.env.PUBLIC_URL}/images/555-california.jpg`}
-          className="object-cover pl-10"
+          className="object-cover w-[80%] ml-auto max-h-[450px] md:w-full"
           alt="Embarcadero Center"
         />
-        <div className="bg-[#CCD9CF] absolute -mt-8 w-5/6 p-6 flex flex-col gap-4">
+        <div className="bg-[#CCD9CF] max-w-[400px] absolute md:relative -mt-8 md:mt-0 w-5/6 p-6 md:p-0 md:bg-transparent flex flex-col justify-center md:min-h-full gap-4">
           <h1 className="text-4xl font-black">SF POPOS</h1>
-          <h2 className="text-2xl font-bold">San Francisco Privately Owned Public Spaces</h2>
+          <h2 className="text-2xl font-bold">San Francisco Privately Owned Public Open Spaces</h2>
+          <div className="hidden md:block">{searchBar}</div>
         </div>
       </div>
-      <form className="flex flex-col my-4 gap-4">
-        <input
-          className="border rounded p-2 w-full"
-          value={query}
-          placeholder="Search for a space..."
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        {/* <button className="bg-gray-100 rounded p-1" type="submit">Search</button> */}
-      </form>
+      <div className="block md:hidden">{searchBar}</div>
       <div className="grid grid-cols-1 min-[500px]:grid-cols-2 min-[800px]:grid-cols-3 min-[1100px]:grid-cols-4 gap-8 min-[800px]:gap-10">
         {spaces}
       </div>
